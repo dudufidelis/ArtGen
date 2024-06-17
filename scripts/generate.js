40ec33b9a3397158ef1a07bd1f0cda6d8de369a6
Eduardo Fidelis <duduecal12@gmail.com>
2024-06-17 14:01:22 -0300

    script reorganizado + htmlToPdf adicionado

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,3 +1,12 @@
+//buttons
+const generatePNGButton = document.getElementById("download-png");
+const generatePDFButton = document.getElementById('download-pdf');
+//Art to generate
+const boasVindas = document.getElementById("BoasVindasTV");
+const papelPorta = document.getElementById("papelPorta");
+
+//inputs
+
 const genre = document.querySelector(".genre");
 const doctorName = document.querySelector(".doctorName");
 const date = document.querySelector(".date");
@@ -5,7 +14,6 @@ const procedure = document.querySelector(".procedure");
 const foodAllergies = document.querySelector(".foodAllergies");
 const drugAllergy = document.querySelector(".drugAllergy");
 const suits = document.querySelector(".suits");
-
 
 function showBoasVindasInputs() {
     genre.style.display = "flex";
@@ -15,6 +23,8 @@ function showBoasVindasInputs() {
     foodAllergies.style.display = "none";
     drugAllergy.style.display = "none";
     suits.style.display = "none";
+    generatePNGButton.style.display = "block";
+    generatePDFButton.style.display = "none";
 }
 
 function showPapelPortaInputs() {
@@ -25,14 +35,13 @@ function showPapelPortaInputs() {
     foodAllergies.style.display = "flex";
     drugAllergy.style.display = "flex";
     suits.style.display = "flex";
+    generatePNGButton.style.display = "none";
+    generatePDFButton.style.display = "block";
 };
 
 function selectArt() {
     const art = document.getElementById("art").value;
-
-    const boasVindas = document.getElementById("BoasVindasTV");
-    const papelPorta = document.getElementById("papelPorta");
-
+    
     if (art === "boasVindas") {
         boasVindas.style.display = "flex";
         papelPorta.style.display = "none";
@@ -47,22 +56,22 @@ function selectArt() {
     }
 };
 
-document.getElementById("download-btn").onclick = function () {
-    const screenshotTarget = document.getElementById("BoasVindasTV");
-    const inputName = document.getElementById("nameInput").value;
+generatePNGButton.addEventListener('click', function() {
+    const inputName = document.getElementById("nameInput").value
     const name = document.getElementById("bv-name");
     const fileName = inputName + '.png';
-    const genre = document.getElementById("genre").value;
     const welcomeMsg = document.getElementById("bv-welcome-msg");
+    const genre = document.getElementById("genre").value;
 
     if (genre === "masc") {
         welcomeMsg.textContent = "Seja Bem-Vindo!";
     } else {
         welcomeMsg.textContent = "Seja Bem-Vinda!";
     }
+
     name.textContent = inputName;
 
-    html2canvas(screenshotTarget, { scale: 10 }).then((canvas) => {
+    html2canvas(boasVindas, { scale: 5 }).then((canvas) => {
         const base64image = canvas.toDataURL("image/png");
         var a = document.createElement("a");
         a.setAttribute("href", base64image);
@@ -70,4 +79,24 @@ document.getElementById("download-btn").onclick = function () {
         a.click();
         a.remove();
     });
-};
+});
+
+generatePDFButton.addEventListener('click', function() {
+    const inputName = document.getElementById("nameInput").value
+    const fileName = inputName + '.pdf';
+    const opt = {
+      filename: fileName,
+      image: { type: 'png', quality: 1 },
+      html2canvas: { scale: 2 },
+      jsPDF: {  
+        unit: 'mm',
+        format: [30, 90],
+        orientation: 'landscape' 
+      }
+    };
+  
+    html2pdf()
+      .set(opt)
+      .from(papelPorta)
+      .save();
+  });
