81799423df40bca3a0e194a6a205e87c1ba4ff69
Eduardo <dudufidelispk@hotmail.com>
2025-05-14 10:47:48 -0300

    new art added

--- scripts/boasVindasGenerator.js
+++ scripts/boasVindasGenerator.js
@@ -1,24 +1,73 @@
+let selectedArt = "hospitalization";
+
+document.querySelector("#artOpt").addEventListener("change", () => {
+    selectedArt = document.querySelector("#artOpt").value;
+    switch (selectedArt) {
+        case "hospitalization":
+            showSelectedArt("hospitalization");
+            break;
+        case "maternity":
+            showSelectedArt("maternity");
+            break;
+    }
+});
+
+function showSelectedArt(selectedArt) {
+    const inputGenre = document.querySelector(".genre");
+    const boas_vindas = document.querySelector("#BoasVindasTV");
+    const maternidade = document.querySelector("#maternidade");
+
+    if (selectedArt === "hospitalization") {
+        maternidade.classList.add("notSelected");
+        inputGenre.classList.remove("notSelected");
+        boas_vindas.classList.remove("notSelected");
+    } else {
+        boas_vindas.classList.add("notSelected");
+        inputGenre.classList.add("notSelected");
+        maternidade.classList.remove("notSelected");
+    }
+}
+
 function getInputValues() {
     const inputName = document.getElementById("nameInput").value;
-    const genre = document.getElementById("genre").value;
+    let genre = "";
+
+    if (selectedArt === "hospitalization") {
+        genre = document.getElementById("genre").value;
+    }
 
     return { inputName, genre };
 }
 
 function updatePreviewDisplay(inputValues) {
-    const name = document.getElementById("bv-name");
-    const welcomeMsg = document.getElementById("bv-welcome-msg");
+    if (selectedArt === "hospitalization") {
+        const bv_name = document.getElementById("bv-name");
+        const welcomeMsg = document.getElementById("bv-welcome-msg");
 
-    welcomeMsg.textContent = inputValues.genre === "masc" 
-        ? "Seja Bem-Vindo!" 
-        : "Seja Bem-Vinda!";
+        if (welcomeMsg) {
+            welcomeMsg.textContent =
+                inputValues.genre === "masc" ? "Seja Bem-Vindo!" : "Seja Bem-Vinda!";
+        }
 
-    name.textContent = inputValues.inputName;
-    name.style.textTransform = "uppercase"
+        if (bv_name) {
+            bv_name.textContent = inputValues.inputName;
+            bv_name.style.textTransform = "uppercase";
+        }
+    } else {
+        const mt_name = document.querySelector(".mt-name");
+
+        if (mt_name) {
+            mt_name.textContent = inputValues.inputName;
+            mt_name.style.textTransform = "capitalize";
+        }
+    }
 }
 
+
 document.getElementById("download-png").addEventListener("click", function () {
-    const boasVindas = document.getElementById("BoasVindasTV");
+    const boasVindas = selectedArt === "hospitalization"
+        ? document.getElementById("BoasVindasTV")
+        : document.getElementById("maternidade");
 
     const inputValues = getInputValues();
     updatePreviewDisplay(inputValues);
@@ -34,3 +83,4 @@ document.getElementById("download-png").addEventListener("click", function () {
         link.remove();
     });
 });
+
